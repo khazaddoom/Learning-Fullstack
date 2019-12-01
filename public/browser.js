42023c938bec4f76bb11ea30c74bd411388e1bfa
khazadoom <31121986g@gmail.com>
2019-12-01 08:03:43 +0530

    sending data from client

--- public/browser.js
+++ public/browser.js
@@ -1,12 +1,13 @@
 document.addEventListener('click', function(e) {
     if (e.target.classList.contains('edit-me')) {
-        let userInput = prompt('Enter the ToDo item text');
-
-        axios.post('/update-item', {text: userInput, id: e.target.getAttribute('data-id') }).then(function() {
-            alert('Succesfully updated the ToDo item.');
-        }).catch(function (err) {
-            console.error(err);
-            alert('Please try again later!')
-        })
+        let userInput = prompt('Enter the ToDo item text', e.target.parentElement.parentElement.querySelector('.item-text').innerHTML);
+        if(userInput) {
+            axios.post('/update-item', {text: userInput, id: e.target.getAttribute('data-id') }).then(function() {
+                e.target.parentElement.parentElement.querySelector('.item-text').innerHTML = userInput
+            }).catch(function (err) {
+                console.error(err);
+                alert('Please try again later!')
+            })
+        }       
     }
 })
