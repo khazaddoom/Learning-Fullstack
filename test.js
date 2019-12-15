044a48f20a0a4f4c0234b4c9601515417ff79038
khazaddoom <31121986g@gmail.com>
2019-12-15 17:18:44 +0530

    testing promises

--- test.js
+++ test.js
@@ -1,32 +1,8 @@
-let express = require('express');
+function calculateSquare(input) {
+    return new Promise((resolve, reject) => {
+        if(typeof input !== 'number') reject('Invalid input type!');
+        resolve(input * input)
+    })
+}
 
-let ourApp = express();
-
-ourApp.use(express.urlencoded({extended: false}));
-
-ourApp.get('/', function(req, res) {
-    res.send(`
-    <form action="/answer" method="POST">
-        <p>What is the color of the sky on a bright sunny day?</p>
-        <input name="answer" autoComplete="off">
-        <button>Submit</button>
-    </form>
-    `);
-});
-
-ourApp.post('/answer', function(req, res) {
-    const answer = (req.body.answer).toLowerCase();
-    if (answer ==='blue') {
-        res.send(`
-            <p>Yes ${req.body.answer}, that is correct!</p>
-            <a href='/'>Back to homepage</a>
-        `);
-    } else {
-        res.send(`
-            <p>That is incorrect. Try again!</p>
-            <a href='/'>Back to homepage</a>
-        `);
-    }
-});
-
-ourApp.listen(3000);
+module.exports = calculateSquare;
